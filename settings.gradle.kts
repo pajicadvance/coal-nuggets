6a1b480037e0c9d2720cb853bd463d9a5e39f46b
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-09-14 17:39:27 +0200

    Bump 26.3 ver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,7 +31,7 @@ stonecutter {
         match("1.21.1", "fabric", "neoforge")
         match("26.1", "fabric", "neoforge", version = "26.1.2")
         match("26.2", "fabric", "neoforge")
-        match("26.3", "fabric", version = "26.3-pre-1")
+        match("26.3", "fabric", version = "26.3-rc-2")
         vcsVersion = "26.2-fabric"
     }
 }
