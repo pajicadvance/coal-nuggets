2e5eee35695ea516ab7789f088057e661e960f98
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-09-23 19:49:47 +0300

    Fix cooking times in 26.3

--- src/main/java/me/pajic/coalnuggets/CoalNuggets.java
+++ src/main/java/me/pajic/coalnuggets/CoalNuggets.java
@@ -6,13 +6,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
-//? >=26.3 {
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.component.CookingFuel;
-import net.minecraft.world.level.storage.loot.providers.number.floats.ContextFloatProviders;
-import net.minecraft.world.level.storage.loot.providers.number.floats.ResolvableFloat;
-import net.minecraft.world.level.storage.loot.providers.number.ints.ResolvableInt;
-//?}
+//? >=26.3
+import net.minecraft.world.level.storage.loot.providers.number.ints.ContextIntProvider;
 
 public class CoalNuggets {
 
@@ -21,18 +16,14 @@ public class CoalNuggets {
     public static final ResourceKey<Item> COAL_NUGGET_KEY = ResourceKey.create(Registries.ITEM, id("coal_nugget"));
     public static final ResourceKey<Item> CHARCOAL_NUGGET_KEY = ResourceKey.create(Registries.ITEM, id("charcoal_nugget"));
     public static final TagKey<Item> COAL_NUGGETS = TagKey.create(Registries.ITEM, id("coal_nuggets"));
+    //? >=26.3
+    public static final ResourceKey<ContextIntProvider> COOKING_TIME_COAL_NUGGETS = ResourceKey.create(Registries.CONTEXT_INT_PROVIDER, id("cooking/time_coal_nuggets"));
 
     public static final Item COAL_NUGGET = new Item(buildProperties(COAL_NUGGET_KEY));
     public static final Item CHARCOAL_NUGGET = new Item(buildProperties(CHARCOAL_NUGGET_KEY));
 
     private static Item.Properties buildProperties(ResourceKey<Item> key) {
-        return new Item.Properties()
-                //? >=26.3 {
-                .component(DataComponents.COOKING_FUEL, new CookingFuel(
-                        new ResolvableInt.Constant(200),
-                        ResolvableFloat.fromKey(ContextFloatProviders.COOKING_DEFAULT_SPEED_MULTIPLIER)))
-                //?}
-                /*? >=26.1 {*/.setId(key)/*?}*/;
+        return new Item.Properties()/*? >=26.3 {*/.cookingFuel(COOKING_TIME_COAL_NUGGETS)/*?}*//*? >=26.1 {*/.setId(key)/*?}*/;
     }
 
     public static void onInitialize() {
